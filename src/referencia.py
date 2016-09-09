ec1079f701b6395bd42213e93f773fcfc4fec783
Csxeba <csxeba@gmail.com>
2016-09-09 13:32:26 +0200

    No smalltalk

---
+++ src/referencia.py
@@ -0,0 +1,13 @@
+def lisatfogado(lista):
+    # Szeretném, ha program úgy írná ki a listát, hogy az "utálom"
+    # helyett a "szeretem" szerepel.
+
+    # kódot!!
+
+    print(lista)
+
+l = ["én", "nagyon", "utálom", "a", "sört"]
+
+fogado(l)
+
+print(l[2] == "utálom")
