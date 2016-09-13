4c8262ed49db4eab6410c1fca1da3744fce8ec3f
Csxeba <csxeba@gmail.com>
2016-09-13 08:43:43 +0200

    OK I'm freezing lesson 00... It's > 200 lines now :S

--- iq/00base.py
+++ iq/00base.py
@@ -1,6 +1,9 @@
 # Ez komment
 
+# -------------------------------
 # Adattípusok, változó deklaráció
+# -------------------------------
+
 a = 2  # integer, vagy egész szám
 b = 2.  # float, vagy lebegőpontos szám
 # floatokról annyit kell tudni, hogy korlátozott a pontosságuk
@@ -8,16 +11,18 @@ b = 2.  # float, vagy lebegőpontos szám
 # double-nek is hívják pl. C/C++-ban (dupla pontosságú)
 igaz = True  # bool, vagy igazság-érték, lehet True és False
 
-# Kiíratás -> műveletek különböző adattípusokkal
-print(a + b)  # 4.0  (mert <b> float)
-print(a + a)  # 4    (mert <a> int)
-print(a + igaz)  # 3    (ilyet is szabad)
-print(b + igaz)  # 3.0  (sőt, ilyet is)
+# -------------------
+# Kiíratás, műveletek
+# -------------------
+print(a + b)  # 4.0  (<b> float, <a> is floattá konvertálódik, így elvégezhető a művelet)
+print(a + a)  # 4    (<a> int, két inttel elvéhezhető a művelet)
+print(a + igaz)  # 3    (A bool konvertálódik intté. A True intként 1, a False intként 0)
+print(b + igaz)  # 3.0  (Hasonlóan az előzőhöz. A True floatként 1.0, a False 0.0)
 
-c = "2"  # ez string, vagy karakterlánc
+c = "2"  # ez a string, vagy karakterlánc
 c += "_"  # összeadás és változó-hozzárendelés egyben
 print(c)  # c értéke itt "2_"
-# Stringeknél két string összeadását konkatenációnak nevezzük
+# Stringeknél két string "összeadását" konkatenációnak nevezzük
 
 print(c * 2)  # 2_2_ (stringet szorozhatsz egésszel)
 c *= 2  # ez konstrukció szorzással is működik
@@ -30,31 +35,82 @@ a += 1  # növeli a értékét 1-gyel, 2-re.
 a += 1
 a += 1  # stb stb stb
 # Ez a kiterjesztett értékadás minden művelettel használható.
-b = 13
 # a műveleti jeleket a programozásban operátoroknak nevezzük
 # Pythonban az alábbi operátorok léteznek:
+b = 13
+
+# ----------
+# Operátorok
+# ----------
+
 print(a + b)  # összeadás, blabla, ezek magától értődők
 print(a - b)
 print(a * b)
-print(b / a)  # valódi osztás, mindig float-ot ad eredményül
-# Egészrész-osztás, int-et ad eredményül.
+print(b / a)  # valódi osztás, mindig float-ot ad eredményül,
+# akkor is, ha inteket osztasz egymással.
+
+# Egészrész-osztás, mindig int-et ad eredményül.
 print(b // a)  # Nem kerekít, hanem levágja a törtrészt
 # Modulo operátor, elvégzi az osztást és nem az eredményt,
 print(b % a)  # hanem a maradékot adja vissza
+# Ezeket általában együtt használjuk:
+egeszresz = b // a
+maradek = b % a
 
-# del kulcsszó változó(k) törlésére
-# ennek nagy programoknál lehet jelentőssége, ha memóriát akarsz felszabadítani
-del a, b, c, igaz
+# ------------
+# del kulcsszó
+# ------------
 
-# többszörös értékadás
-a = b = 1  # ilyet is lehet, bár én nem ajánlom, majd később meglátod, miért
+del a, b, c, igaz, maradek, egeszresz
+# törli a megadott változó(ka)t
+# ennek nagy programoknál lehet jelentőssége,
+# ha memóriát akarsz felszabadítani
 
-# Valamicskét a kiíratásról:
-# Pythonban a print() függvénnyel íratunk ki.
+# -------------
+# Függvényhívás
+# -------------
+
+# Írok pár szót a függvényekről, mert most már fogunk olyanokat használni.
+# Matematikai szempontból a függvény valami, aminek átadsz egy vagy több
+# valamit és visszaad egy valamit.
+# Így jelöljük: függvény(valami1, valami2, ...) -> vissza_valami
+# A "vissza_valami"-t elkaphatod egy változóval:
+# változó = függvény(valami1, valami2, ...)
+
+# Amiket átadunk, azokat argumentumoknak vagy paramétereknek nevezzük.
+# Ami kijön, az a visszatérési érték.
+
+# Programozásban még olyan függvényekkel is találkozunk, amiknek nincs
+# visszatérési értéke, csak átadsz neki argumentumokat (vagy nem adsz neki semmit)
+# valamit csinál, és nem tér vissza semmivel.
+# A valódi visszatérési értékkel rendelkező függvényeket nevezzük függvényeknek,
+# azokat, amiknek nincs visszatérési értéke, metódusoknak vagy eljárásoknak.
+
+# A print() egy eljárás, nem tér vissza semmivel (illetve visszatér egy None értékkel)
+# A következőképpen néz ki az argumentumlistája:
+# print(*objects, sep=" ", end="\n", file=None, flush=None)
+# Mit jelent ez?
+# Huh bonyolult. Nézzünk meg egy egyszerűbbet :D
+# A printre majd visszatérünk.
+
+# Az input() egy valódi függvény, a usertől lehet bemenetet kérni vele.
+# Így néz ki az argumentumlistája:
+# input(prompt)
+# Úgy működik, hogy kiírja a promptot, majd vár, míg be nem írsz valamit és entert
+# nem ütsz. Utána !stringként! visszatér azzal, amit a user beírt. (A számot is
+# stringként adja vissza.)
+
+# Egy függvény hívásakor az argumentumokat sorban átadhatjuk, vagy nevesíthetjük is
+# input("> ") ugyanaz, mint
+# input(prompt="> ")
+# A nevesíthető argumentumokat kulcsszó-argumentumnak is hívjuk. Gyakran rövidítik
+# kw vagy kwargként, az anonim argumentumokat arg/args-nak szokták rövidíteni.
+
+# Na vissza a printhez, elsőre egyszerű megközelítéssel:
 print(a)
-# A print függvénynek több (névtelen) argumentuma is lehet:
+# A print eljárásnak több (névtelen) argumentuma is lehet:
 print("a:", a, "b:", b)
-# Van két nevesített (vagy kulcsszó-) argumentuma is:
+# Van pár nevesített (vagy kulcsszó-) argumentuma is:
 # sep a szeparátor karakter(lánc), amit a vesszők helyére betesz.
 print("a:", a, "b:", b, sep="\t")  # tabot adtam meg (alapból szóköz " ")
 # end a legvégéhez ad hozzá:
@@ -63,21 +119,29 @@ print("b:", b, end=" ")
 print()  # üres print, hogy új sort kezdjünk
 # Az üres print azért kezd új sort, mert az end kulcsszó-argumentum default értéke "\n"
 
+# Látható, hogy az inputtal ellentétben a kulcsszó-argumentumok csak explicit megnevezés
+# útján érhetőek el, mivel
 
-# többszörös párhuzamos értékadás
+
+# --------------------
+# Többszörös értékadás
+# --------------------
+
+a = b = 1  # ilyet is lehet, bár én nem ajánlom, majd később meglátod, miért
+del a, b
+
+# Ez a forma jobb, ha már tömöríteni akar az ember
 a, b, c, d = 1, 2, 3, 4
 e, f, g, h = 4, 1, 2, 3
 i, j, k, l = 3, 4, 1, 2
 m, n, o, p = 2, 3, 4, 1
 
-# string beolvasás
-end = input(prompt="Wha? > ")  # input függvénnyel kérhetsz a usertől bemenetet.
-# Az input függvénynek egy argumentuma van, a prompt.
-# Úgy működik, hogy kiírja a promptot, majd vár, míg be nem írsz valamit és entert
-# nem ütsz. Utána visszaadja !stringként! azt, amit a user beírt. A számot is
-# stringként adja vissza.
+# --------------------
+# String beolvasás
+# --------------------
+end = input(prompt="Mi van? > ")  # input függvénnyel kérhetsz a usertől bemenetet.
 
-# Kis string manipuláció:
+# String manipuláció:
 end += "\n"  # <end> végére konkatenáljuk az új sor karaktert
 end = "\t" + end  # <end> elejére konkatenálunk egy tabot
 
@@ -92,4 +156,55 @@ print(e, f, g, h, sep="\t", end=end)
 print(i, j, k, l, sep="\t", end=end)
 print(m, n, o, p, sep="\t", end=end)
 
-# TODO: írni még a castolásról!!!
+# --------------------------
+# Castolás avagy konvertálás
+# --------------------------
+
+# ez sokszor implicit megtörténik (pl felül a print automatikusan konvertálja
+# a számokat stringekké és azokat írja ki.
+# intet probléma nélkül castolhatsz float-ra:
+a = 1
+float_a = float(a)
+print(float_a)
+# ha float-ot castolsz int-re, akkor kerekítés történik:
+a = 1.5
+int_a = int(a)
+print(int_a)
+
+# stringre castolni bármit lehet probléma nélkül, simán megkapod a string reprezentációt:
+print(str(float_a))
+print(str(int_a))
+# De ugye azt írtam feljebb, hogy a print() implicit (automatikusan) is elvégzi ezt a
+# konverziót, így ugyanazt az eredményt kapod, mintha simán átadtán neki nyersen a számokat.
+
+# Ez is érdekes:
+igaz = True
+print(igaz)  # történik implicit konverzió
+print(str(igaz))  # itt explicit konvertálunk
+# A True-t is implicit konvertálja a print() és kapsz egy "True" stringet
+
+# --------------
+# Round függvény
+# --------------
+
+# Egyébként float-ot tudsz tetszőleges tizedesjegyre kerekíteni a round() függvénnyel, ekkor
+# nem intet kapsz vissza, hanem kerekítve kapod meg a floatodat.
+# A roundnak két argumentuma van: number és ndigits. A numbert kötelező megadni,
+# az ndigits-nek van default értéke, ami None. Ha defaulton hagyod, akkor egészre kerekít,
+# de akkot is float-ot kapsz vissza és nem intet.
+a = 3.33333
+a = round(number=a, ndigits=2)
+print(a, end=" Hello Szabi\n")
+# A fenti 3 utasítás egy sorban :)
+print(round(3.33333, 2), end=" Hello Szabi\n")
+
+# Stringeket is lehet számokká konvertálni, ez főleg akkor hasznos, ha
+# input()-tal bekérsz valamit. Az input() mindig stringet ad vissza,
+# azt pedig vissza kell alakítani számmá.
+# Ha a konverzió elvégezhető, akkor semmi probléma, viszont egyes eseteket
+# (pl. Troll Szabolcs szöveget ír be számok helyett) szükséges lehet kezelni
+# Próbáld ki, törd el az inputot:
+szabi_mond_egy_szamot = input("Na milyen számot mond Szabi?\n> ")
+x = float(szabi_mond_egy_szamot)
+
+# A trollok kikerülését majd megtanítom.
--- iq/01if.py
+++ iq/01if.py
@@ -1,48 +1,86 @@
+# Egy pár szó a logikai elágazásról (if)
+# A logikai elágazás a pythonban összetett utasítás, ami azt jelenti, hogy
+# több soros is lehet. Ezt szerintem minden nyelvben így van (mármint hogy
+# az IF több soros). Az ilyen összetett utasítások használatánál jelezni
+# kell, hogy hol kezdődik egy utasítás-blokk és hol végződik.
+# Ezt C/C++-ban, C#-ban, Java-ban pl. {} kapcsos zárójellel jelölik.
+# Pythonban ezt behúzással jelöljük (tab)
+# Mivel az összetett utasítások egymásba is ágyazhatóak (tehát ifen belül
+# if, azon belül újabb if, stb.), ez jó nagy káoszhoz tud vezetni.
+
 a = 1
 b = 1.
 c = "1"
-d = True
 
 # int, float és bool viszonya egymáshoz logikai kifejezésekben
 print("-"*30)  # Dísznek
+# (Figyeld a behúzást!)
 if a == b:
-    print("a == b: <a> got implicitly casted to float")
-    print("1. == 1. indeed")
+    print("a == b: <a> implicit floattá konvertálódott")
+    print("1. == 1. valóban")
     print("-"*30)
 
 if a == c:
     print("This won't run")
     print("-"*30)
 
-if a == d:
-    print("a == d: This does work, <a> gets casted to bool")
-    print("True == True indeed")
+# Összehasonlító operátorok:
+# ==, <, <=, >, >=, magától értődőek.
+# != a nem egyenlő
+# Amit még fogsz használni: not kulcsszó:
+if not False:
+    print("A nem hamis az igaz :)")
+# A not kulcsszó mindig egy logikai kifejezés elejére kerül.
+
+# Adatok igazságértékéről pár szó:
+# amikor valamit bool-lá castolsz, akkor azt mondjuk,
+# hogy az adat igazságértékét kéred le.
+# Konvenció alapján mindennek, ami nem 0, az igazságértéke True
+# Ez minden programnyelven így van, nem csak Pythonban.
+# Tehát ami nem igaz: 0, 0.0, "" (üres string) és majd még meglátod mik :)
+# A speciális None érték igazságértéke is False
+print(bool(None))
+
+# Megjegyzendő, hogy az if implicit konvertál minden kifejezést bool-ra,
+# amit átadsz neki.
+# Pl. ezek teljesen érvényes konstrukciók:
+if a:
+    print("A igaz bool értékké konvertálódott implicit módon.")
+elif not a:
+    print("Ez az ág nem fog futni, mert negáltuk az igazzá konvertálódott <a>-t")
+
+
+igaz = True
+
+if a == igaz:
+    print("a == igaz: <a> bool-lá konvertálódik")
+    print("True == True valóban")
     print("-"*30)
 
 # Melyik ág fut? Miért?
-if c == d:
-    print("Weird. c == d evaluates to:", c == d)
-    print("Why is that?")
+if c == igaz:
+    print("Fura. c == igaz értéke:", c == igaz)
+    print("Miért van ez?")
     print("-"*30)
 else:
-    print("Weird. c == d evaluates to:", c == d)
-    print("Why is that?")
+    print("Fura. c == igaz értéke:", c == igaz)
+    print("Miért van ez?")
     print("-"*30)
 
 z = 0
 
-if z == d:
-    print("This won't run of course")
+if z == igaz:
+    print("Természetesen ez nem fut, mivel a 0 igazsságértéke False")
 elif not z:  # not kulcsszó a logikai kifejezés elejére kell, hogy kerüljön
     print("<z> implicitly gets casted to bool")
-    print("not False is True indeed")
+    print("not False is True valóban")
     print("-"*30)
 else:
     print("This won't run because we entered the elif branch above")
 
 if b > z:
     print("b > z: integers and floats can be compared")
-    print("1. is greater than 0 indeed.")
+    print("1. is greater than 0 valóban.")
     print("-"*30)
 
 message = "I like beer. Mmmmm!!!"
