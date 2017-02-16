94c49c4dd3ffcee50b940962189554e21f3e4917
Csxeba <csxeba@gmail.com>
2017-02-16 22:20:02 +0100

    Adding katas

---
+++ kata/stringek.py
@@ -0,0 +1,25 @@
+"""
+Most stringeket fogunk manipulálni.
+Referencia IQ:
+A szeleteléshez a 02lista.py
+A string osztály metódusai: 06minden_objektum.py
+"""
+
+
+def feladat1(datum):
+    """
+    Írj egy függvényt, ami egy stringként átadott
+    dátumot átalakít ev, ho, nap egészekké és azok
+    listájával tér vissza.
+    Tehát ha '2016.02.16.'-ot kap, akkor a
+    [2016, 2, 16] listával tér vissza, aminek
+    minden eleme int típusú.
+    """
+    pass
+
+
+def feladat2():
+    """
+    A data/fejvagyiras/kiserlet.txt fájlban kockadobásokat
+
+    """
